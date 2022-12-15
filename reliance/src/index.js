ebf1d8ae3c7cd78afe3848cc12002a2c13c4dbc2
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-12-15 22:07:39 +0530

    day3

--- reliance/src/index.js
+++ reliance/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
-
-import { BrowserRouter } from 'react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>

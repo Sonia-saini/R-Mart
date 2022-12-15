c61b9154dc5f1c2d0a4dedbe34a6b1807744bc7e
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-12-15 22:25:25 +0530

    bootstrap

--- reliance/src/index.js
+++ reliance/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

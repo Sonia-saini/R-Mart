77308a9a593880cb9135b23dc4336bbd9caadbe7
sumansauravmay <sumansauravmay12345@gmail.com>
2022-12-16 16:42:58 +0530

    day-3

--- reliance/src/App.js
+++ reliance/src/App.js
@@ -9,20 +9,22 @@ import "./App.css";
 // import logo from './logo.svg';
  import React from "react";
 
-import Admin from "./component/suman/Admin";
-import Login from './component/suman/Login';
- import Otp from './component/suman/Otp';
-import Register from './component/suman/Register';
+// import Admin from "./component/suman/Admin";
+// import Login from './component/suman/Login';
+//  import Otp from './component/suman/Otp';
+// import Register from './component/suman/Register';
+import {Home} from "./component/sonia/home"
 
 function App() {
   return (
 
     <div > 
-    <h1>Hey Group</h1>
-       <Register/>
+   
+       {/* <Register/>
        <Otp/>
        <Login/> 
-       <Admin/>
+       <Admin/> */}
+       <Home/>
     </div>
   );
 }

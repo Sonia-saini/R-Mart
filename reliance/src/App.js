b8c9f880a447f127d66f4095724ee37dc4a849db
sumansauravmay <sumansauravmay12345@gmail.com>
2022-12-16 11:29:08 +0530

    fw18_0575_day-3

--- reliance/src/App.js
+++ reliance/src/App.js
@@ -1,16 +1,16 @@
 // import logo from './logo.svg';
  import React from "react";
 import Admin from "./component/suman/Admin";
-// import Login from './component/suman/Login';
-//  import Otp from './component/suman/Otp';
-// import Register from './component/suman/Register';
+import Login from './component/suman/Login';
+ import Otp from './component/suman/Otp';
+import Register from './component/suman/Register';
 
 function App() {
   return (
     <div > 
-       {/* <Register/>
+       <Register/>
        <Otp/>
-       <Login/> */}
+       <Login/>
        <Admin/>
     </div>
   );

dcde8aca55ba90886e4d135fb0b3a0c00d470322
TheLegendaryWarrior <sanket.potdar563@gmail.com>
2022-12-18 04:14:44 +0530

    Cart Page Completed

--- reliance/src/component/sanket/styles/themeConfig.js
+++ reliance/src/component/sanket/styles/themeConfig.js
@@ -1,3 +1,44 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({});
+const activeLabelStyles = {
+  color: "blue.500",
+  transform: "scale(0.85) translateY(-24px)"
+};
+
+export const theme = extendTheme({
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles
+              }
+            },
+            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
+              {
+                ...activeLabelStyles
+              },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              fontSize: "sm",
+              alignSelf: "normal",
+              fontWeight: "normal",
+              position: "absolute",
+              pointerEvents: "none",
+              color: "blackAlpha.600",
+              backgroundColor: "white",
+              mx: 3,
+              px: 1,
+              my: 2,
+              transformOrigin: "left top"
+            }
+          }
+        }
+      }
+    }
+  }
+});
--- reliance/src/index.js
+++ reliance/src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { theme } from "./component/sanket/styles/themeConfig";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </Provider>

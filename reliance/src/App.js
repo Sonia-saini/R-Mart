47b597c33a7f8ecad7e6d63ea1e49f4fa6de4d9b
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-12-18 16:49:28 +0530

    main

--- reliance/src/App.js
+++ reliance/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Makeup from './component/mitali/allRoute/makeup/makeup'
-import Iron from './component/mitali/allRoute/mobiles/mobiles'
-import AllRoutes from './component/mitali/allRoute/allRoutes'
+
+
+import Allroutes from './router/allroutes';
 
 function App() {
   return (
     <div className="App">
      {/* <Makeup/>
      <Iron/> */}
-     <AllRoutes/>
+    <Allroutes/>
     </div>
   );
 }

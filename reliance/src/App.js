ad5361b90fe1ffa2cf2ac8a3d59fe86e8fe556d7
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-12-18 16:54:21 +0530

    main

--- reliance/src/App.js
+++ reliance/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 
 import Allroutes from './router/allroutes';
+import Footer from './component/Saurabh/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      {/* <Makeup/>
      <Iron/> */}
     <Allroutes/>
+    <Footer/>
     </div>
   );
 }
